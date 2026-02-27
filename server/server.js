fea5b7b09419139b13142a88cee6cd12f95369a1
NasimKT <abdulnasimkt@gmail.com~>
2026-02-28 05:20:30 +0530

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,25 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import connectDB from "./config/db.js";
+
+import authRoutes from "./routes/auth.js";
+import contactRoutes from "./routes/contacts.js";
+import sosRoutes from "./routes/sos.js";
+
+dotenv.config();
+connectDB();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/contacts", contactRoutes);
+app.use("/api/sos", sosRoutes);
+
+
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
+});
